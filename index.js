d75798ad3b2b8056e125828883e5cc22106480e1
Marvyan <125864076+Marvyan@users.noreply.github.com>
2023-10-21 19:49:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+//const hbs = require("html")
+const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const tempelatePath = path.join(__dirname, '../tempelates')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
+//app.set('view engine', 'hbs')
+app.set('views', tempelatePath)
+app.use(express.static(publicPath))
+
+
+// hbs.registerPartials(partialPath)
+
+
+app.get('/signup.', (req, res) => {
+    res.render('signup')
+})
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+
+
+// app.get('/home', (req, res) => {
+//     res.render('home')
+// })
+
+app.post('/signup', async (req, res) => {
+    
+    // const data = new LogInCollection({
+    //     name: req.body.name,
+    //     password: req.body.password
+    // })
+    // await data.save()
+
+    const data = {
+        email: req.body.email,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        gender: req.body.gender,
+        country: req.body.country
+    }
+
+    const checking = await LogInCollection.findOne({ email: req.body.email })
+
+   try{
+    if (checking.name === req.body.name && checking.password===req.body.password) {
+        res.send("user details already exists")
+    }
+    else{
+        await LogInCollection.insertMany([data])
+    }
+   }
+   catch{
+    res.send("wrong inputs")
+   }
+
+    res.status(201).render("home", {
+        naming: req.body.name
+    })
+})
+
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ email: req.body.email })
+
+        if (check.password === req.body.password) {
+            res.status(201).render("home", { naming: `${req.body.password}+${req.body.email}` })
+        }
+
+        else {
+            res.send("incorrect password")
+        }
+
+
+    } 
+    
+    catch (e) {
+
+        res.send("wrong details")
+        
+
+    }
+
+
+})
+
+
+
+app.listen(port, () => {
+    console.log('port connected');
+})
